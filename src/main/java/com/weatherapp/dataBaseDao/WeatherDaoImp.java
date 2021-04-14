5464814563a2488845c2259665452f3dfc89c9b1
garczynskiDominik <garczynski.dominik@gmail.com>
2021-04-14 22:39:30 +0200

    Add method to update id_Localization in Weather table

--- src/main/java/com/weatherapp/dataBaseDao/WeatherDaoImp.java
+++ src/main/java/com/weatherapp/dataBaseDao/WeatherDaoImp.java
@@ -4,6 +4,7 @@ import com.weatherapp.connection.HibernateUtils;
 import com.weatherapp.model.Weather;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 public class WeatherDaoImp implements WeatherDao {
     @Override
@@ -18,5 +19,33 @@ public class WeatherDaoImp implements WeatherDao {
 
         session.getTransaction().commit();
         session.close();
+    }
+
+    @Override
+    public void saveIdLocalization(long id) {
+
+        Session session = HibernateUtils
+                .getInstance()
+                .getSessionFactory()
+                .getCurrentSession();
+        session.beginTransaction();
+
+
+        String queryDataBase = new StringBuilder("update weather set localization_id=")
+                .append(id)
+                .append(" where id=")
+                .append(id)
+                .toString();
+        Query query = session.createNativeQuery(queryDataBase);
+//        Query query = session.createQuery("update Weather set localization_id=:idLocalization where id=:id")
+//                .setParameter("idLocalization", id)
+//                .setParameter("id", id);
+
+
+        int a = query.executeUpdate();
+
+        session.getTransaction().commit();
+        session.close();
+
     }
 }
